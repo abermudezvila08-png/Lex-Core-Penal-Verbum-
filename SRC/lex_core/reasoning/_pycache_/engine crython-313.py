656d1ed64bccfdb44c76698039f0c78ce2c7f025
Armando Omar <abermudezvila08@gmail.com>
2026-08-30 11:03:07 -0400

    Create engine crython-313.py

---
+++ SRC/lex_core/reasoning/_pycache_/engine crython-313.py
@@ -0,0 +1,62 @@
+ó
+
+    ê
+”j    ã                    ó0   • S S K J r J r J r J r     " S   S 5       r g ) é    ) Ú ElementÚ ElementStateÚ ResultÚ Rulec                    ó4   • \ r S r S r S \ S \ \ \ 4     S \ 4 S   j r	S r
+g ) Ú
+RuleEngineé    Ú ruleÚ elementsÚ returnc                    óþ   • / n / n / n / n U R                     HÒ  n U R                   U 5       n U (       d   U R                   U 5         M.  U R                   U 5         U R                   [         R
+                  :X  a   M_  U R                   [         R                   :X  a   U R                   U 5         M�  U R                   [         R                   :X  a   U R                   U 5         MÁ  U R                   U 5         MÔ  
+   U (       d   U (       d   U (       d   S n	U R                   n
+O S n	S n
+U   V
+s / s   H   n
+S U
+  3 P M
+  
+   n n
+XÄ  V
+s / s   H   n
+S U
+  3 P M
+  
+   s n
+-
+  n XÅ  V
+s / s   H   n
+S U
+  3 P M
+  
+   s n
+-
+  n [         U
+U U R                   / U U	S 9 $ s   s n
+f s   s n
+f s   s n
+f ) NÚ
+compatibleÚ no_concluyenteuZ   No procede una conclusiÃ³n jurÃ­dica definitiva con los elementos actualmente acreditados.z Elemento pendiente: z Elemento controvertido: z"Elemento parcialmente acreditado: ) Ú
+conclusionÚ fundamentalsÚ rules_executedÚ
+uncertaintiesÚ status)
+Ú required_elementsÚ getÚ appendÚ stater    Ú	SATISFIEDÚ DISPUTEDÚ PARTIALÚ
+consequencer    Ú id)
+Ú selfr
+   r
+   Ú missingÚ disputedÚ partialÚ usedÚ
+element_idÚ elementr    r    Ú xr    s
+                Ú9/mnt/data/Tu-aboga2-cuba/src/lex_core/reasoning/engine.pyÚ evaluateÚ RuleEngine.evaluate    s^   € Ø  ˆ Ø  ˆ Ø  ˆ Ø  ˆ Ø  × 0Ô 0ˆJØ  —l‘l :Ó .ˆGÞ  Ø  — ‘ ˜zÔ *Ù  Ø  �K‰K˜
+Ô #Ø  �}‰}¤ × 6Ñ 6Ó 6Ù  Ø  �}‰}¤ × 5Ñ 5Ó 5Ø  — ‘  
+Ö +Ø  — ‘ ¤,×"6Ñ"6Ó 6Ø  — ‘ ˜zÖ *à  — ‘ ˜zÖ *ñ   1ö    žx¶ Ø !ˆFØ  × )Ñ )‰Jà %ˆFØ uˆJá=DÓ EºW¸ Ð /° ¨sÓ 3¹Wˆ
+Ð EØ  À(Ó KÂ(¸QÐ 4°Q°CÓ 8Á(Ñ KÑ Kˆ
+Ø  ÈGÓ TÊGÀqÐ >¸q¸cÓ BÉGÑ TÑ Tˆ
+Ü  Ø !Ø  Ø  ŸG™G˜9Ø 'Ø  ñ
+  
+ð  	
+ùò   F ùÚ KùÚ Ts    Ä  E0 Ä, E5 Å  E: © N)
+Ú __name__Ú
+__module__Ú __qualname__Ú __firstlineno__r    Ú dictÚ strr    r    r'   Ú __static_attributes__r)   ó    r&   r    r        s%   † ð $ 
+˜Tð $ 
+¨T°#°w°,Ñ-?ð $ 
+ÀF÷ $ 
+r1   r    N) Ú
+domain.modelsr    r    r    r    r    r)   r1   r&   Ú <module>r3       s    ð    ß ?Ó ?÷ % 
+ò % 
+r1   
+
